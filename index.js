72dd88009e0ed8182a6e4039ef9821d148ff2218
Vivek vardhan <1998vivekvardhan@gmail.com>
2022-08-04 01:19:26 +0530

    changed code to easy instead of using fontAwesome

--- index.js
+++ index.js
@@ -7,18 +7,10 @@ if(window.performance.getEntriesByType("navigation")[0].type==="reload"){
     document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+".png");
 
     if(randomNumber1>randomNumber2){
-        var temp = document.createElement("script")
-        document.querySelector("head").appendChild(temp)
-        document.firstElementChild.firstElementChild.querySelector("script").setAttribute('src','https://kit.fontawesome.com/83f0db8b9d.js');
-        document.firstElementChild.firstElementChild.querySelector("script").setAttribute('crossorigin','anonymous');
-        document.querySelector("h1").innerHTML = "<i class='fa-brands fa-apple' id='fg'></i> Player 1 Wins!"
+        document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!"
     }
     else if(randomNumber1<randomNumber2){
-        var temp = document.createElement("script")
-        document.querySelector("head").appendChild(temp)
-        document.firstElementChild.firstElementChild.querySelector("script").setAttribute('src','https://kit.fontawesome.com/83f0db8b9d.js');
-        document.firstElementChild.firstElementChild.querySelector("script").setAttribute('crossorigin','anonymous');
-        document.querySelector("h1").innerHTML = "Player 2 Wins! <i class='fa-brands fa-apple' id='fg'></i>"
+        document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩"
     }
     else{
         document.querySelector("h1").textContent="Draw!";
